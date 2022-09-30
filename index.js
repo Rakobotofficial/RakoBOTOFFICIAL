d0e706890520d0e47cd85e010e4ec0f71c8c8841
Dek Wahyu <114536508+Rakobotofficial@users.noreply.github.com>
2022-09-30 09:41:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
 const BotName = 'RakoBOT[OFFICIAL]'; 
-const instagram = 'https://instagram.com/reuz.id'; 
+const instagram = 'https://instagram.com/dekwahyu2022'; 
 const telegram = 'https://t.me/reuzzz'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
 const youtube = 'https://www.youtube.com/c/reuz_id';
